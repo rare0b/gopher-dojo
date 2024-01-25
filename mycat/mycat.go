4bb7ebf960df6ea8308e3945dd90638de925bdf7
rare0b <rare0b0110@gmail.com>
2024-01-26 08:51:37 +0900

    wip

--- mycat/mycat.go
+++ mycat/mycat.go
@@ -3,10 +3,19 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
+func init() {
+	flag.String("n", "", "files")
+}
+
 func main() {
-	args := flag.Args()
-	fmt.Println(args)
+	flag.Parse()
+
+	flagArgs := flag.Args()
+	osArgs := os.Args
+	fmt.Println(flagArgs)
+	fmt.Println(osArgs)
 	fmt.Println("Hello")
 }
